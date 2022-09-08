42744fb26ec279ec6183178ea7da9a9d47b98248
trantung <tranductung.hhtt@gmail.com>
2022-09-08 14:12:39 +0700

    validation sign-in

--- src/component/Auth/Login/Signin.js
+++ src/component/Auth/Login/Signin.js
@@ -1,23 +1,57 @@
 import { useDispatch } from "react-redux/es/exports";
 import { loginUser } from "../../../utils/apiRequest";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import "./login.scss"
 function Signin({changeAuthMode}) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [usename, setusername] = useState("");
+  const [disable,setDisable]=useState(true)
+
+  const [username, setusername] = useState("");
   const [password, setPassword] = useState("");
+  const onChangeUsername=(e)=>{
+    setusername(e.target.value.trim());
+  }
+  const onChanePassword=(e)=>{
+    setPassword(e.target.value.trim());
+  }
   const handleSubmitLogin = (e) => {
     e.preventDefault();
     const user = {
-      username: usename,
+      username: username,
       password: password,
     };
     loginUser(user, dispatch, navigate);
   };
+
+
+
+  const formik =useFormik({
+    initialValues:{
+      username:"",
+      password:"",
+    },
+    validationSchema:Yup.object({
+      username:Yup.string().required("Reqiued"),
+      password:Yup.string().required().min(6,'min 6 char')
+    }),
+    onSubmit:(user)=>{
+      // console.log(values)
+      loginUser(user, dispatch, navigate);
+    }
+  });
+  useEffect(()=>{
+    // console.log(formik.errors)
+    if(formik.errors ==={}) console.log('true')
+  },[formik.errors])
+  // console.log(!formik.errors)
+
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form" onSubmit={handleSubmitLogin}>
+      <form className="Auth-form" onSubmit={formik.handleSubmit}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="text-center">
@@ -29,33 +63,38 @@ function Signin({changeAuthMode}) {
           <div className="form-group mt-3">
             <label>User name</label>
             <input
+              id="username"
               type="text"
+              
               className="form-control mt-1"
               placeholder="User name"
-              onChange={(e) => {
-                setusername(e.target.value);
-              }}
+              value={formik.values.username}
+              onChange={formik.handleChange}
             />
+            {
+              (formik.errors.username) && (<label className="text-error">{formik.errors.username}</label>)
+            }
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
             <input
+              id="password"
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+              value={formik.values.password}
+              onChange={formik.handleChange}
             />
+            {
+                (formik.errors.password) && (<label className="text-error">{formik.errors.password}</label>)
+              }
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" disabled={!formik.isValid}>
               Submit
             </button>
           </div>
-          <p className="text-center mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
+         
         </div>
       </form>
     </div>
